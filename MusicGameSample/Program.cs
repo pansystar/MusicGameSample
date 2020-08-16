e73499ab81505efa2392283f6083cc44337d63cf
Pansystar <wh.bt3.p@gmail.com>
2020-08-16 10:17:04 +0900

    初期コミット

---
+++ MusicGameSample/Program.cs
@@ -0,0 +1,28 @@
+﻿using static DxLibDLL.DX;
+
+namespace MusicGameBPMSample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ChangeWindowMode(1);
+
+			DxLib_Init();
+
+			while(ProcessMessage() == 0)
+			{
+				ClearDrawScreen();
+
+				if(CheckHitKey(KEY_INPUT_ESCAPE) == 1)
+				{
+					break;
+				}
+
+				ScreenFlip();
+			}
+
+			DxLib_End();
+		}
+	}
+}
